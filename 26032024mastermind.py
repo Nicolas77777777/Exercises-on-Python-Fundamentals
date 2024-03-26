9e1f423e9440ae75d071bf11cda5ac1139722c3d
Nicolas77777777 <sairwater@gmail.com>
2024-03-26 12:15:43 +0100

    MASTER MIND

---
+++ 26032024mastermind.py
@@ -0,0 +1,21 @@
+import random
+
+
+import copy
+import random
+
+
+# Genera una lista che contiene M numeri casuali tra 1 e N SEgreto 
+def GeneraLista(N, M):
+    chisono="sono Il master mind"
+    lista = []
+    for i in range(M):
+        v = random.randint(1, N)
+        lista.append(v)
+    return lista
+
+def GeneraLista (N,M)
+
+
+
+
